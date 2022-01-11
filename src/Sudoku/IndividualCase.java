eebace7a529171baca268b1b638bb22910ab1f32
Alain Gires Ebouel nzom <egires@yahoo.fr>
2022-01-11 12:28:02 -0500

    commit

--- src/Sudoku/IndividualCase.java
+++ src/Sudoku/IndividualCase.java
@@ -1,27 +1,172 @@
 package Sudoku;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class IndividualCase
 {
-	private TextField textField ;
-	private int id ;
+	private TextField bigTextField;
+	TextField textField1;
+	TextField textField2;
+	TextField textField3;
+	TextField textField4;
+	TextField textField5;
+
+	VBox littleFaces;
+	HBox vbox1;
+	HBox vbox2;
+	HBox vbox3;
+	private int id;
 	private static final String textfieldClass = "textfield";
-	
-	
+	private static final String littlefieldClass = "littlefield";
+
+	private StackPane stackPane;
+
 	public IndividualCase(int id)
 	{
-		this.textField = new TextField();
 		this.id = id;
-		textField.getStyleClass().add(textfieldClass);
+
+		init();
+
+		bigTextField.getStyleClass().add(textfieldClass);
+
+		stackPane.getChildren().addAll(littleFaces, bigTextField);
+
+		littleFaces.toFront();
+
+		CreateLittleCssClass();
+
 	}
+
+	public TextField getBigTextField()
+	{
+		return bigTextField;
+	}
+
+	public void setBigTextField(TextField bigTextField)
+	{
+		this.bigTextField = bigTextField;
+	}
+
+	public TextField getTextField1()
+	{
+		return textField1;
+	}
+
+	public void setTextField1(TextField textField1)
+	{
+		this.textField1 = textField1;
+	}
+
+	public TextField getTextField2()
+	{
+		return textField2;
+	}
+
+	public void setTextField2(TextField textField2)
+	{
+		this.textField2 = textField2;
+	}
+
+	public TextField getTextField3()
+	{
+		return textField3;
+	}
+
+	public void setTextField3(TextField textField3)
+	{
+		this.textField3 = textField3;
+	}
+
+	public TextField getTextField4()
+	{
+		return textField4;
+	}
+
+	public void setTextField4(TextField textField4)
+	{
+		this.textField4 = textField4;
+	}
+
+	public TextField getTextField5()
+	{
+		return textField5;
+	}
+
+	public void setTextField5(TextField textField5)
+	{
+		this.textField5 = textField5;
+	}
+
+	public VBox getLittleFaces()
+	{
+		return littleFaces;
+	}
+
+	public void setLittleFaces(VBox littleFaces)
+	{
+		this.littleFaces = littleFaces;
+	}
+
+	public StackPane getStackPane()
+	{
+		return stackPane;
+	}
+
+	public void setStackPane(StackPane stackPane)
+	{
+		this.stackPane = stackPane;
+	}
+
+	private void init()
+	{
+		this.stackPane = new StackPane();
+
+		bigTextField = new TextField();
+
+		this.textField1 = new TextField();
+		this.textField2 = new TextField();
+
+		this.textField3 = new TextField();
+
+		textField3.setAlignment(Pos.CENTER);
+
+		this.textField4 = new TextField();
+		this.textField5 = new TextField();
+
+		littleFaces = new VBox();
+
+		this.vbox1 = new HBox();
+		this.vbox2 = new HBox();
+		this.vbox3 = new HBox();
+
+		littleFaces.getChildren().addAll(vbox1, vbox2, vbox3);
+		vbox1.getChildren().addAll(textField1, textField2);
+		vbox2.getChildren().addAll(textField3);
+		vbox3.getChildren().addAll(textField4, textField5);
+
+	}
+
+	private void CreateLittleCssClass()
+	{
+		textField1.getStyleClass().add(littlefieldClass);
+		textField2.getStyleClass().add(littlefieldClass);
+		textField3.getStyleClass().add(littlefieldClass);
+		textField4.getStyleClass().add(littlefieldClass);
+		textField5.getStyleClass().add(littlefieldClass);
+	}
+
 	public TextField getTextField()
 	{
-		return textField;
+		return bigTextField;
 	}
+
 	public int getId()
 	{
 		return id;
 	}
-	
+
 }
--- src/Sudoku/LitltleSudoku.java
+++ src/Sudoku/LitltleSudoku.java
@@ -7,18 +7,17 @@ import javafx.scene.layout.VBox;
 
 public class LitltleSudoku
 {
-	private ArrayList <IndividualCase> individualCaseList;
-	private HBox hbox = new HBox();	
+	private ArrayList<IndividualCase> individualCaseList;
+	private HBox hbox = new HBox();
 	private VBox vbox1 = new VBox();
 	private VBox vbox2 = new VBox();
 	private VBox vbox3 = new VBox();
 	private static final String LsudokuClass = "LsudokuClass";
-	
+
 	public ArrayList<IndividualCase> getIndividualCaseList()
 	{
 		return individualCaseList;
-		
-		
+
 	}
 
 	public HBox getHbox()
@@ -41,28 +40,27 @@ public class LitltleSudoku
 		return vbox3;
 	}
 
-	
 	public LitltleSudoku()
 	{
 		individualCaseList = new ArrayList<>();
 		init(vbox1);
 		init(vbox2);
 		init(vbox3);
-		
+
 		hbox.getChildren().addAll(vbox1, vbox2, vbox3);
 		hbox.getStyleClass().add(LsudokuClass);
-		
+
 	}
 
 	private void init(VBox vbox)
 	{
-		for(int i = 0; i < 3; i++)
+		for (int i = 0; i < 3; i++)
 		{
 			IndividualCase indCase = new IndividualCase(Sudoku.getIndividualCaseId());
 			individualCaseList.add(indCase);
-			vbox.getChildren().add(indCase.getTextField());	
-			
+			vbox.getChildren().add(indCase.getStackPane());
+
 		}
-	}	
+	}
 
 }
