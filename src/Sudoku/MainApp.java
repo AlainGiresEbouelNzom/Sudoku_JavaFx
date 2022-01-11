86aa586d242fe9dcc6209190064ac19126689119
ALAIN GIRES EBOUEL NZOM <egires@yahoo.fr>
2022-01-11 05:22:47 -0500

    First commit

---
+++ src/Sudoku/MainApp.java
@@ -0,0 +1,23 @@
+package Sudoku;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application
+{
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		
+		
+		
+		primaryStage.show();
+		
+	}
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module Sudoku
+{
+	requires javafx.controls;
+	requires transitive  javafx.graphics ;
+	exports Sudoku;
+}
